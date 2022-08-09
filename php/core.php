85ed05831a6f88d09623ac703fad15a4f6196cca
Anthony Funes <gioant1@hotmail.com>
2022-08-09 14:14:27 -0400

    made the following changes in these 2 files

--- php/core.php
+++ php/core.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 
-	include "C:/wamp6/www/TeamProject/classes/DB_Manager.class.php";
-    include "C:/wamp6/www/TeamProject/classes/Users.class.php";
+include_once "../classes/DB_Manager.class.php";
+include_once "../classes/Users.class.php";
 
     $database = new DB_Manager();
 /*
