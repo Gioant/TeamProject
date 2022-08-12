77c4260ab3bd19382d0db442bcc3b1d6809164e5
Gioant <gioant1@hotmail.com>
2022-08-11 20:15:03 -0400

    Andy completely redesigned edit profile form & added ability to display Avatar from DB using their ID

--- admin/editprofile.php
+++ admin/editprofile.php
@@ -17,77 +17,6 @@
         <script src="js/scripts.js"></script>
     </head>
     <body class="bg-secondary">
-<!--        <div id="layoutAuthentication">-->
-<!--            <div id="layoutAuthentication_content">-->
-<!--                <main>-->
-<!--                    <div class="container">-->
-<!--                        <div class="row justify-content-center">-->
-<!--                            <div class="col-lg-7">-->
-<!--                                <div class="card shadow-lg border-0 rounded-lg mt-5">-->
-<!--                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Edit Profile</h3></div>-->
-<!--                                    <div class="card-body">-->
-<!--                                        <form method="POST" action="#" enctype="multipart/form-data">-->
-<!--                                            <div class="row mb-3">-->
-<!--                                                <div class="col-md-6">-->
-<!--                                                    <div class="form-floating mb-3 mb-md-0">-->
-<!--                                                        <input class="form-control" id="inputFirstName" type="text" name="edit_firstname" placeholder="Enter your first name" value="--><?php //echo $_SESSION['firstname'] ?><!--" />-->
-<!--                                                        <label for="inputFirstName">First name</label>-->
-<!--                                                    </div>-->
-<!--                                                </div>-->
-<!--                                                <div class="col-md-6">-->
-<!--                                                    <div class="form-floating">-->
-<!--                                                        <input class="form-control" id="inputLastName" type="text" name="edit_lastname" placeholder="Enter your last name" value="--><?php //echo $_SESSION['lastname'] ?><!--"/>-->
-<!--                                                        <label for="inputLastName">Last name</label>-->
-<!--                                                    </div>-->
-<!--                                                </div>-->
-<!--                                            </div>-->
-<!--                                            <div class="col-md-12 mb-3">-->
-<!--                                                <div class="form-floating">-->
-<!--                                                    <input class="form-control" id="inputUsername" type="text" name="edit_username" placeholder="Enter your username" value="--><?php //echo $_SESSION['username'] ?><!--"/>-->
-<!--                                                    <label for="inputUsername">Username</label>-->
-<!--                                                </div>-->
-<!--                                            </div>-->
-<!--                                            <div class="col-md-12 mb-3">-->
-<!--                                                <div class="form-control">-->
-<!--                                                    <label for="avatar">Avatar:&emsp;</label>-->
-<!--                                                    <input type="text" disabled value="--><?php //echo $_SESSION['avatar'] ?><!--" >-->
-<!--                                                    <input type="file" name="edit_avatar" id="avatar">-->
-<!--                                                </div>-->
-<!--                                            </div>-->
-<!--                                            <div class="form-floating mb-3">-->
-<!--                                                <input class="form-control" id="inputEmail" type="email" name="edit_email" placeholder="name@example.com" value="--><?php //echo $_SESSION['email'] ?><!--"/>-->
-<!--                                                <label for="inputEmail">Email address</label>-->
-<!--                                            </div>-->
-<!--                                            <div class="row mb-3">-->
-<!--                                                <div class="col-md-6">-->
-<!--                                                    <div class="form-floating mb-3 mb-md-0">-->
-<!--                                                        <input class="form-control" id="inputPassword" type="password" name="edit_password" placeholder="Create a password" minlength="5" value="--><?php //echo $_SESSION['password'] ?><!--" />-->
-<!--                                                        <label for="inputPassword">Password</label>-->
-<!--                                                    </div>-->
-<!--                                                </div>-->
-<!--                                            </div>-->
-<!--                                            <div class="mt-4 mb-0">-->
-<!--                                                <div class="d-grid"><input class="btn btn-primary btn-block" type="submit" name="edit_profile" value="Edit Profile"></div>-->
-<!--                                            </div>-->
-<!--                                        </form>-->
-<!--                                    </div>-->
-<!--                                </div>-->
-<!--                            </div>-->
-<!--                        </div>-->
-<!--                    </div>-->
-<!--                </main>-->
-<!--            </div>-->
-<!--            <div id="layoutAuthentication_footer">-->
-<!--                <footer class="py-4 bg-light mt-auto">-->
-<!--                    <div class="container-fluid px-4">-->
-<!--                        <div class="d-flex align-items-center justify-content-between small">-->
-<!--                            <div class="text-muted">Copyright &copy; Team-C the best team in the world</div>-->
-<!--                        </div>-->
-<!--                    </div>-->
-<!--                </footer>-->
-<!--            </div>-->
-<!--        </div>-->
-
 
 <!-- Wrapper -->
 <div class="content-wrapper" style="margin-top: 35px;">
@@ -101,13 +30,13 @@
                     <div class="card-body">
                         <div class="d-flex align-items-start align-items-sm-center gap-4">
                             <!-- Show Picture -->
-                            <img src="../assets/img/avatars/1.png" alt="user-avatar" class="d-block rounded" id="uploadedAvatar" width="100" height="100">
+                            <img src="./image/<?php echo $_SESSION['pic']; ?>" alt="user-avatar" class="d-block rounded" id="uploadedAvatar" width="100" height="100">
                             <div class="button-wrapper">
                                 <!-- Upload Input-->
                                 <label for="upload" class="btn btn-primary me-2 mb-4" tabindex="0">
                                     <span class="d-none d-sm-block">Upload new photo</span>
                                     <i class="bx bx-upload d-block d-sm-none"></i>
-                                    <input type="file" id="upload" class="account-file-input" accept="image/png, image/jpeg" hidden="">
+                                    <input type="file" id="upload" class="account-file-input" accept="image/png, image/jpeg" hidden="" name="userPic">
                                 </label>
                                 <!--Reset Button -->
                                 <button type="button" class="btn btn-outline-secondary account-image-reset mb-4">
@@ -120,7 +49,7 @@
                     </div>
                     <hr class="my-0">
                     <div class="card-body">
-                        <form id="formAccountSettings" method="POST" onsubmit="return false">
+                        <form id="formAccountSettings" method="POST" action="#" enctype="multipart/form-data">
                             <div class="row">
                                 <!-- First Name -->
                                 <div class="mb-3 col-md-6">
@@ -145,7 +74,7 @@
                                 <!--Password -->
                                 <div class="mb-3 col-md-6">
                                     <label for="Password" class="form-label">Password</label>
-                                    <input type="text" class="form-control" id="Password" name="password">
+                                    <input type="text" class="form-control" id="Password" name="Pwd">
                                 </div>
                                 <!-- Level -->
                                 <div class="mb-3 col-md-6">
@@ -154,7 +83,7 @@
                                 </div>
                             </div>
                             <div class="mt-2">
-                                <button type="submit" class="btn btn-primary me-2">Save changes</button>
+                                <button type="submit" class="btn btn-primary me-2" name="edit">Save changes</button>
                                 <button type="reset" class="btn btn-outline-secondary">Cancel</button>
                             </div>
                         </form>
