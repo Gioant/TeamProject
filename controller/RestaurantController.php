3d4788b0fc4e9a36cf8b358c05eb397c1a346ba4
Gioant <gioant1@hotmail.com>
2022-08-15 13:33:58 -0400

    Andy Added a separate Model & Controller For Restaurant

---
+++ controller/RestaurantController.php
@@ -0,0 +1,11 @@
+<?php
+//function to load required model classes
+spl_autoload_register(function ($class_name) {
+    $filename = $class_name . '.class.php';
+    include_once '../model/' . $filename;
+});
+
+session_start();
+$database = new DB_Manager();
+
+
