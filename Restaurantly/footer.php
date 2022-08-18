08e3ec7e3711e5f2cf92ef75cdd70ac048480619
Gioant <gioant1@hotmail.com>
2022-08-17 21:33:34 -0400

    Nick Provided Footer Logic. Andy Created 1 footer file to be included in all other pages

---
+++ Restaurantly/footer.php
@@ -0,0 +1,29 @@
+<!-- ======= Footer ======= -->
+<footer id="footer">
+    <div class="footer-top">
+        <div class="container">
+            <div style="display: flex;justify-content: space-between">
+                <div class="col-lg-3 col-md-6">
+                    <div class="footer-info">
+                        <h3>Content Changer</h3>
+                        <p>
+                            1616 René-Lévesque Blvd W<br>
+                        <p>
+                            Montreal, Quebec H3H 1P8<br><br>   </p>
+                        <p> <strong>Phone:</strong> +1 5589 55488 55<br>   </p>
+                        <p><strong>Email:</strong> info@example.com<br>   </p>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-6 footer-links">
+                    <h4>Useful Links</h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="index.php">Home</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="about.php">About us</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="services.php">Services</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="portfolio.php">Portfolio</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="contact.php">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
